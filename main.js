85e739dbd050f461fe95e42a13e4ee6b6d6f82d8
AngemiriNetwork <53487163+AngemiriNetwork@users.noreply.github.com>
2019-07-31 13:57:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function showSlides(n) {
 }
 //no.2
 
-var slideIndex = 1;
+var slideIndex = 2;
 showSlides1(slideIndex);
 
 function plusSlides(n) {
@@ -57,7 +57,7 @@ function showSlides1(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 2}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
