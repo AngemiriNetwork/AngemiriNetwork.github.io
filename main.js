f8bee3d62a2ffd72500432cdef5a8b1409c4bb6c
AngemiriNetwork <53487163+AngemiriNetwork@users.noreply.github.com>
2019-07-31 11:27:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function openCity(evt, cityName) {
 
 
 
-var slideIndex = [1,1];
+var slideIndex = [0,1];
 var slideId = ["mySlides1", "mySlides2"]
 showDivs(1, 0);
 showDivs(1, 1);
@@ -35,7 +35,22 @@ function showDivs(n, no) {
 }
 
 
-var myIndex = 1;
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+var myIndex = 2;
 carousel();
 
 function carousel() {
